5e67e3f73882585c482c3eecbb6849b407210483
Mithun4450 <132843404+Mithun4450@users.noreply.github.com>
2024-08-11 17:48:09 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// example-1
+function showDate(){
+    document.getElementById('s_date').innerHTML=Date();
+}
+
+// example-2
+function turnOnBulb(){
+    document.getElementById('bulb').src="images/bulbon.gif";
+}
+function turnOffBulb(){
+    document.getElementById('bulb').src="images/bulboff.gif";
+}
+
+// example-3
+function makeBig(){
+    document.getElementById('make_big').style.fontSize = "60px";
+}
+
+// example-4
+function hideText(){
+    document.getElementById('hide').style.display = "none";
+}
+
+
